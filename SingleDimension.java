d33c3b68d5bfd5e5b8e74fea269cd18edbde6043
Ravi sahani <108450291+Ravicsecap@users.noreply.github.com>
2022-07-28 11:44:37 +0530

    Add files via upload

---
+++ SingleDimension.java
@@ -0,0 +1,16 @@
+
+public class SingleDimension {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[]a=new int[3];
+       a[0]=25;
+       a[1]=83;
+       a[2]=256;
+       System.out.println("1D array example:-");
+       System.out.println(a[0]);
+       System.out.println(a[1]);
+       System.out.println(a[2]);
+	}
+
+}
